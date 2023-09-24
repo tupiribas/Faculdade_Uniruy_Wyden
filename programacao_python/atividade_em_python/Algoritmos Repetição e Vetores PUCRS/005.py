79d6d6162a0fee5938f2cacc317b194c67d1144c
tupiribas <tupiguedes@outlook.com>
2023-09-24 10:36:23 -0300

    Repetição e Vetores PUCRS - 005

--- programacao_python/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/005.py
+++ programacao_python/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/005.py
@@ -5,3 +5,23 @@ valores inteiros positivos, lidos externamente.
 O final da leitura acontecerá quando for lido um valor negativo.
 '''
 
+soma_valores_internos = 0
+quantidade_valores_internos = 0
+valores_internos = 0
+
+while True:
+    try:
+        valores_internos = int(input('Digite um número: \n'))
+
+        if valores_internos < 0:
+            # Fechar programa
+            break
+
+        quantidade_valores_internos += 1
+        soma_valores_internos += valores_internos
+    except ValueError:
+        # Caso o usuário insira um valor inválido
+        print('Digite um número válido.')
+
+media_aritimetica = soma_valores_internos / quantidade_valores_internos
+print(f'A média aritimétrica desses números: {media_aritimetica}')
