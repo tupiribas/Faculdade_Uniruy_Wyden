d32b815a7a52a047cd2bab7424f36259e64697b3
tupiribas <tupiguedes@outlook.com>
2023-09-24 10:26:51 -0300

    Melhorando variáveis e organizando documentação - 003

--- programacao_python/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/003.py
+++ programacao_python/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/003.py
@@ -13,47 +13,49 @@ coletando dados sobre o salário e número de filhos. A prefeitura deseja saber:
 
 print('Pesquisa da Prefeitura de Ruy Barbosa')
 
-soma = 0.0
-cont_salario = 0
-cont_num_filho = 0
+total_salario = 0.0
+perc_salario_ate_100 = 0
+total_filhos = 0
 maior_salario = 0.0
 percentual = 0.0
-i = 0
+quantidade_pessoas = 0
 while True:
     try:
         salario = float(input('Qual o valor da sua renda mensal? '))
         
-        if salario < 0: 
+        if salario < 0: # Fecha o programa quando digitado valor negativo em salário
             break
-        else: 
-            soma += salario
-            # Percentual de salários menores que 100
-            if salario <= 100.0:
-                print('e menor que 100')
-                cont_salario += 1
 
-        # Maior salario
-        if salario > maior_salario:
-            maior_salario = salario
-        
         num_filhos = int(input('Quantos filhos você tem? '))
 
-        if num_filhos < 0: # Não conta com idade negativa errados 
+        total_salario += salario
+        quantidade_pessoas += 1
+
+        # Não conta com idade negativa errados 
+        if num_filhos > 0:
+            total_filhos += num_filhos
+        else:
             print('A quantidade de filhos está incorreta.')
-        else: cont_num_filho += num_filhos
 
-        i += 1
-    except ValueError: # Caso o usuário insira uma letra
-        print('Digite um valor váido \n')
+        # Obter o percentual de salários menores que 100
+        if salario <= 100.0:
+            perc_salario_ate_100 += 1
 
-media_salarial = soma / i
-media_num_filhos = cont_num_filho / i
-percentual_salario = (cont_salario / i) * 100
+        # Obter o maior salario
+        if salario > maior_salario:
+            maior_salario = salario
+    except ValueError: 
+        # Caso o usuário insira um valor inválido
+        print('Digite um número válido \n')
+
+media_salarial = total_salario / quantidade_pessoas
+media_num_filhos = total_filhos / quantidade_pessoas
+perc_salario_ate_100 = (perc_salario_ate_100 / quantidade_pessoas) * 100
 
 print(f'''
 Média de salários: R${media_salarial:.2f}
 Média do números de filhos: {media_num_filhos} 
 Maior salário: {maior_salario}
-Percentual de salários menores que R$100.00: {percentual_salario:.0f}%
+Percentual de salários menores que R$100.00: {perc_salario_ate_100:.0f}%
 Programa finalizado.
 ''')
---
+++ programacao_python/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/005.py
@@ -0,0 +1,7 @@
+'''
+Construir um algoritmo que calcule a média aritmética de vários 
+valores inteiros positivos, lidos externamente. 
+
+O final da leitura acontecerá quando for lido um valor negativo.
+'''
+
