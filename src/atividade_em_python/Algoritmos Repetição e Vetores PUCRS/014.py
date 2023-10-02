d0cbdec3eea140975b44439d0f7ecf9530b5fd79
tupiribas <tupiguedes@outlook.com>
2023-10-01 21:22:49 -0300

    Repetição e Vetores PUCRS - 014

--- src/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/014.py
+++ src/atividade_em_python/Algoritmos Repetição e Vetores PUCRS/014.py
@@ -21,23 +21,20 @@ while True:
         media = SOMA/(len(numeros))
 
         # Quantidade de valores positivos e negativos
-        QUANTIDADE_POSITIVO = 0
-        QUANTIDADE_NEGATIVO = 0
-        MAIOR = 0
-        MENOR = 0
+        SOMA_POSITIVO = 0
+        SOMA_NEGATIVO = 0
         for n in numeros:
             MENOR = 0
             if n > 0:
-                QUANTIDADE_POSITIVO += 1
+                SOMA_POSITIVO += 1
             elif n < 0:
-                QUANTIDADE_NEGATIVO += 1
-            
+                SOMA_NEGATIVO += 1
 
         # Percentual positivo e negativo
-        percent_positivo_negativo = ((MAIOR - MENOR) / MENOR) * 100
+        percent_negativos = (SOMA_NEGATIVO / len(numeros)) * 100
+        percent_positivos = (SOMA_POSITIVO / len(numeros)) * 100
 
-        print(
-            f'Média aritmética {media:.2f}\nQuantidade de valores positivos {QUANTIDADE_POSITIVO}\nQuantidade de valores negativos {QUANTIDADE_NEGATIVO}\nPercentual de valores negativos e positivos {percent_positivo_negativo}%')
+        print(f'Média aritmética {media:.2f}\nQuantidade de valores positivos {SOMA_POSITIVO}\nQuantidade de valores negativos: {SOMA_NEGATIVO}\nPercentual de valores negativos: {percent_negativos:.2f}% \nPercentual de valores positivos: {percent_positivos:.2f}%\n')
     except ValueError:
         # Caso o usuário insira um valor inválido
         print('Digite um número válido.')
